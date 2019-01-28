3663e28ba74467b42b0e2eb93bd6dc7f52a1df9b
Martin <martin.baumann99@protonmail.com>
2019-01-28 21:12:50 +0100

    Implemented Insertion Sort Class

---
+++ SortingAlgorithms_CS/Classes/InsertionSort.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlgorithms_CS.Classes
+{
+    static class InsertionSort
+    {
+
+        public static int[] Sort(int[] inputList)
+        {
+            List<int> newList = new List<int>();
+
+            newList.Add(inputList[0]);
+
+            for(int i=1;i<inputList.Length;i++)
+            {
+                for(int u = i-1; u >= 0; u--)
+                {
+                    if (inputList[i] > newList[u])
+                    {
+                        newList.Insert(u + 1, inputList[i]);
+                        break;
+                    }
+                    else if (u == 0) newList.Insert(0, inputList[i]);
+                    
+                }
+            }
+
+            return newList.ToArray();
+        }
+        
+    }
+}
--- SortingAlgorithms_CS/Program.cs
+++ SortingAlgorithms_CS/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SortingAlgorithms_CS.Classes;
 
 namespace SortingAlgorithms_CS
 {
@@ -10,6 +11,41 @@ namespace SortingAlgorithms_CS
     {
         static void Main(string[] args)
         {
+            InsertionSortTest();
+
+            Console.ReadKey();
         }
+
+        static void InsertionSortTest()
+        {
+            Random rnd = new Random();
+
+            int[] unsortedList = new int[9];
+
+            for (int i = 0; i < unsortedList.Length; i++)
+            {
+                unsortedList[i] = rnd.Next(0, 101);
+            }
+
+
+            int[] sortedList = InsertionSort.Sort(unsortedList);
+
+            Console.WriteLine("---Insertion Sort---");
+            Console.WriteLine("Unsorted list: ");
+
+            foreach (int i in unsortedList)
+            {
+                Console.Write(i + " ");
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("Sorted list: ");
+
+            foreach (int i in sortedList)
+            {
+                Console.Write(i + " ");
+            }
+        }
+
     }
 }
