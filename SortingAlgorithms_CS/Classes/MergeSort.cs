53d3520929401d8f09bf1800496be46643839143
Martin <martin.baumann99@protonmail.com>
2019-02-03 15:26:32 +0100

    Implemented MergeSort and time measuring

---
+++ SortingAlgorithms_CS/Classes/MergeSort.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlgorithms_CS.Classes
+{
+    static class MergeSort
+    {
+        public static int[] Sort(int[] input)
+        {
+            if (input.Count() <= 1) return input;
+            
+            int[] left = input.Where((c, i) => i % 2 == 0).ToArray();
+            int[] right = input.Where((c, i) => i % 2 != 0).ToArray();
+            
+            left = Sort(left);
+
+            right = Sort(right);
+
+            return Merge(left, right);
+        }
+
+        private static int[] Merge(int[] left, int[] right)
+        {
+            List<int> result = new List<int>();
+
+            int leftIndex = 0;
+            int rightIndex = 0;
+
+            while (leftIndex < left.Count() && rightIndex < right.Count())
+            {
+                if (left[leftIndex] <= right[rightIndex])
+                {
+                    result.Add(left[leftIndex]);
+                    leftIndex++;
+                }
+                else
+                {
+                    result.Add(right[rightIndex]);
+                    rightIndex++;
+                }
+            }
+
+            if (leftIndex < left.Count())
+            {
+                
+                for (int i = leftIndex; i < left.Count(); i++)
+                {
+                    result.Add(left[i]);
+                }
+
+            }
+            else if(rightIndex < right.Count())
+            {
+                for (int i = rightIndex; i < right.Count(); i++)
+                {
+                    result.Add(right[i]);
+                }
+            }
+
+            return result.ToArray();
+        }
+
+    }
+}
--- SortingAlgorithms_CS/Program.cs
+++ SortingAlgorithms_CS/Program.cs
@@ -11,27 +11,33 @@ namespace SortingAlgorithms_CS
     {
         static void Main(string[] args)
         {
-            InsertionSortTest();
-
-            Console.ReadKey();
-        }
-
-        static void InsertionSortTest()
-        {
+                       
             Random rnd = new Random();
 
-            int[] unsortedList = new int[9];
+            int[] unsortedList = new int[20000];
 
             for (int i = 0; i < unsortedList.Length; i++)
             {
                 unsortedList[i] = rnd.Next(0, 101);
             }
+            
+            InsertionSortTest(unsortedList);
 
+            MergeSortTest(unsortedList);
+
+            Console.ReadKey();
+        }
+
+        static void InsertionSortTest(int[] unsortedList)
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
 
             int[] sortedList = InsertionSort.Sort(unsortedList);
 
+            watch.Stop();
+
             Console.WriteLine("---Insertion Sort---");
-            Console.WriteLine("Unsorted list: ");
+            /*Console.WriteLine("Unsorted list: ");
 
             foreach (int i in unsortedList)
             {
@@ -44,7 +50,41 @@ namespace SortingAlgorithms_CS
             foreach (int i in sortedList)
             {
                 Console.Write(i + " ");
+            }*/
+
+            Console.WriteLine("");
+            Console.Write("Time taken: ");
+            Console.Write(watch.ElapsedMilliseconds + "ms\n");
+        }
+
+        static void MergeSortTest(int[] unsortedList)
+        {
+
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+
+            int[] sortedList = MergeSort.Sort(unsortedList);
+
+            watch.Stop();
+
+            Console.WriteLine("---Merge Sort---");
+            /*Console.WriteLine("Unsorted list: ");
+
+            foreach (int i in unsortedList)
+            {
+                Console.Write(i + " ");
             }
+
+            Console.WriteLine("");
+            Console.WriteLine("Sorted list: ");
+
+            foreach (int i in sortedList)
+            {
+                Console.Write(i + " ");
+            }*/
+
+            Console.WriteLine("");
+            Console.Write("Time taken: ");
+            Console.Write(watch.ElapsedMilliseconds + "ms\n");
         }
 
     }
